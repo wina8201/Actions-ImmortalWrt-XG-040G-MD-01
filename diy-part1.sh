9e91332a3e19962d7c5a1961a4fc75f657a73036
wina8201 <wina8201@gmail.com>
2026-05-31 14:57:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,9 +5,9 @@ USER_BRANCH="$1"   # 例如 master-XG-040G-MD 或 openwrt-25.12-XG-040G-MD
 echo "用户选择分支: $USER_BRANCH"
 
 if [ "$USER_BRANCH" = "master-XG-040G-MD" ]; then
-    PATCH_FILE="$PATCHES_DIR/airoha-en7581-support-master-fzs209-260531.patch"
+    PATCH_FILE="$PATCHES_DIR/airoha-en7581-support-master.patch"
 elif [ "$USER_BRANCH" = "openwrt-25.12-XG-040G-MD" ]; then
-    PATCH_FILE="$PATCHES_DIR/airoha-en7581-support-25.12-fzs209-260531.patch"
+    PATCH_FILE="$PATCHES_DIR/airoha-en7581-support-25.12.patch"
 else
     echo "未知分支，跳过补丁"
     exit 0
